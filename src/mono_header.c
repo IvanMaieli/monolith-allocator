b4393cbd447a7713855d2282129b79cd33a32981
IvanMaieli <ivan.maieli@gmail.com>
2025-11-13 23:14:12 +0100

    Added mono_init() function that initialize the page for the program and the starting pointer to the arena.

--- src/mono_header.c
+++ src/mono_header.c
@@ -1,13 +1,17 @@
+#define _GNU_SOURCE
+#define DIM_ARENA 4096
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
 /*
  * A @mono_block is defined in this way:
  * 
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
- * | mono_header |    Memory in use    |
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * | mono_header |        Memory in use       |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
  * 
  * In this way every block of the @arena, i.e.
  * the memory available to the process, is splitted
@@ -29,11 +33,62 @@
  * @next: the next block of memory in the free list
  */
 
-typedef struct mono_block {
-    size_t dimension;
-    int free;
-    struct mono_block *next;
+typedef struct mono_header {
+    size_t dim;
+    int8_t free;
+    struct mono_header *next;
+} mono_header;
+
+// Starting pointer of the arena (full page allocated for the process)
+void *arena = NULL;
+
+/*
+ * Request di @arena, i.e. all the space allocated for
+ * the specific process, then creates the @mh (mono_header)
+ * pointing at the beginning of the @arena, then sets
+ * the fields of the header as a whole one single free page
+ * of memory, prefixed by an header. 
+ */
+void mono_init() {
+    arena = mmap(NULL, DIM_ARENA, 
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS,
+                       -1, 0);
+    mono_header *mh = (mono_header *) arena;
+    mh->dim = DIM_ARENA - sizeof(mono_header); 
+    mh->free = 1;
+    mh->next = NULL;
 }
+
+
+/*
+ * This function returns a pointer to the allocated memory requested.
+ * The new header is set with offset 'new_header + requested_mem + 1_byte' 
+ * (idc about casting in this docs, imagine that everything is in bytes, not
+ * chunks of mem of structs), so this creates the allocation and translates 
+ * the free memory ahead. 
+ */
+void *mono_loc(size_t usize) {
+    mono_header *mh = (mono_header *) arena;
+    if (mh->dim < (usize + sizeof(mono_header)))         
+        return NULL;
+
+    mono_header *next_h = (mono_header *) ((int8_t *) mh + sizeof(mono_header) + usize);
+    next_h->dim = mh->dim - sizeof(mono_header) - usize;
+    next_h->free = 1;
+    next_h->next = NULL;
+
+    mh->dim = usize;
+    mh->free = 0;
+    mh->next = next_h;
+
+    return (void *) (mh + 1);
+}
+
+int main(void){ return 0; }
+
+
+
 
 
 
